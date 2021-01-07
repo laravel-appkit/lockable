e6d11b36f4ae7aba61dff06ccd38a45226b87ae9
Darren Coutts <couttsdarren@outlook.com>
2021-01-07 14:13:29 +0000

    update test name

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,23 +2,11 @@
 
 namespace AppKit\Lockable\Tests;
 
-use AppKit\Lockable\Tests\Models\Article;
-
 class ExampleTest extends TestCase
 {
     /** @test */
     public function trueIsTrue()
     {
         $this->assertTrue(true);
-    }
-
-    /** @test */
-    public function artcilesCanBeLoaded()
-    {
-        // create 5 articles
-        factory(Article::class, 5)->create();
-
-        // check the database for 5 articles
-        $this->assertEquals(5, Article::count());
     }
 }
